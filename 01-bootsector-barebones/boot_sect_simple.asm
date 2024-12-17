dd3ca83b1ecf2bfdb0bb5ef56e7276374863e1df
JohanBCEkberg <johanbcekberg@gmail.com>
2024-12-17 14:08:31 +0100

    lesson 1 done

--- 01-bootsector-barebones/boot_sect_simple.asm
+++ 01-bootsector-barebones/boot_sect_simple.asm
@@ -1,6 +1,8 @@
-; A simple boot sector program that loops forever
+; Infinite loop (e9 fd ff)
 loop:
-    jmp loop
+    jmp loop 
 
+; Fill with 510 zeros minus the size of the previous code
 times 510-($-$$) db 0
-dw 0xaa55
+; Magic number
+dw 0xaa55 
